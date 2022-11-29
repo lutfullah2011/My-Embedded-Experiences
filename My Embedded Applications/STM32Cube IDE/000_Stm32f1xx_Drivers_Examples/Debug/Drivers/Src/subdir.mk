20c1bdc6aca43bd2665b78af40015fc2dbe8267a
Muhammed Lütfullah AYDIN <70660749+lutfullah2011@users.noreply.github.com>
2022-11-29 20:47:53 +0300

    Add files via upload

---
+++ My Embedded Applications/STM32Cube IDE/000_Stm32f1xx_Drivers_Examples/Debug/Drivers/Src/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Drivers/Src/stm32f100xx_gpio.c 
+
+OBJS += \
+./Drivers/Src/stm32f100xx_gpio.o 
+
+C_DEPS += \
+./Drivers/Src/stm32f100xx_gpio.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/Src/%.o Drivers/Src/%.su: ../Drivers/Src/%.c Drivers/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DSTM32VLDISCOVERY -DSTM32 -DSTM32F1 -DSTM32F100RBTx -c -I../Inc -I"C:/Users/luty1/OneDrive/Belgeler/Embedded-C/My_workspace/target/001Led_Toggle_API/Drivers/Inc" -I"C:/Users/luty1/OneDrive/Belgeler/Embedded-C/My_workspace/target/001Led_Toggle_API/Drivers/Src" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-Drivers-2f-Src
+
+clean-Drivers-2f-Src:
+	-$(RM) ./Drivers/Src/stm32f100xx_gpio.d ./Drivers/Src/stm32f100xx_gpio.o ./Drivers/Src/stm32f100xx_gpio.su
+
+.PHONY: clean-Drivers-2f-Src
+
---
+++ My Embedded Applications/STM32Cube IDE/000_Stm32f1xx_Drivers_Examples/Debug/Src/002Led_button.d
@@ -0,0 +1,7 @@
+Src/002Led_button.o: ../Src/002Led_button.c \
+ C:/Users/luty1/OneDrive/Belgeler/Embedded-C/My_workspace/target/001Led_Toggle_API/Drivers/Inc/stm32f1xx.h \
+ C:/Users/luty1/OneDrive/Belgeler/Embedded-C/My_workspace/target/001Led_Toggle_API/Drivers/Inc/stm32f100xx_gpio.h \
+ C:/Users/luty1/OneDrive/Belgeler/Embedded-C/My_workspace/target/001Led_Toggle_API/Drivers/Inc/stm32f1xx.h
+C:/Users/luty1/OneDrive/Belgeler/Embedded-C/My_workspace/target/001Led_Toggle_API/Drivers/Inc/stm32f1xx.h:
+C:/Users/luty1/OneDrive/Belgeler/Embedded-C/My_workspace/target/001Led_Toggle_API/Drivers/Inc/stm32f100xx_gpio.h:
+C:/Users/luty1/OneDrive/Belgeler/Embedded-C/My_workspace/target/001Led_Toggle_API/Drivers/Inc/stm32f1xx.h:
