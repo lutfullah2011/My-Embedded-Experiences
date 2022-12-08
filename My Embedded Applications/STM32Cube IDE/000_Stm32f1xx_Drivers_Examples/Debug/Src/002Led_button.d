5855a196a33315470e18c2e587d0f6814f9a3770
Muhammed Lütfullah AYDIN <70660749+lutfullah2011@users.noreply.github.com>
2022-12-08 17:02:45 +0300

    Delete My Embedded Applications/STM32Cube IDE/000_Stm32f1xx_Drivers_Examples directory

--- My Embedded Applications/STM32Cube IDE/000_Stm32f1xx_Drivers_Examples/Debug/Src/002Led_button.d
+++
@@ -1,7 +0,0 @@
-Src/002Led_button.o: ../Src/002Led_button.c \
- C:/Users/luty1/OneDrive/Belgeler/Embedded-C/My_workspace/target/001Led_Toggle_API/Drivers/Inc/stm32f1xx.h \
- C:/Users/luty1/OneDrive/Belgeler/Embedded-C/My_workspace/target/001Led_Toggle_API/Drivers/Inc/stm32f100xx_gpio.h \
- C:/Users/luty1/OneDrive/Belgeler/Embedded-C/My_workspace/target/001Led_Toggle_API/Drivers/Inc/stm32f1xx.h
-C:/Users/luty1/OneDrive/Belgeler/Embedded-C/My_workspace/target/001Led_Toggle_API/Drivers/Inc/stm32f1xx.h:
-C:/Users/luty1/OneDrive/Belgeler/Embedded-C/My_workspace/target/001Led_Toggle_API/Drivers/Inc/stm32f100xx_gpio.h:
-C:/Users/luty1/OneDrive/Belgeler/Embedded-C/My_workspace/target/001Led_Toggle_API/Drivers/Inc/stm32f1xx.h:
